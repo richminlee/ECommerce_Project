9a08022b9300f49a70ab977c57fada59b2596e49
Richard Lee <lee.richardm@gmail.com>
2020-09-15 19:18:36 -0700

    Initial Commit

---
+++ ECommerceProject/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerceProject.Models;
+
+namespace ECommerceProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
