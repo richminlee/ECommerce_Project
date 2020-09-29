3a568aee98cd4abbd66ab9dfecf1e0d6696df85c
Richard Lee <lee.richardm@gmail.com>
2020-09-28 20:31:24 -0700

    added admin

--- ECommerceProject/Models/IStoreRepository.cs
+++ ECommerceProject/Models/IStoreRepository.cs
@@ -8,5 +8,8 @@ namespace ECommerceProject.Models
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
     }
 }
